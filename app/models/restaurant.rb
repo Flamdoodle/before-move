de1065eca2b95b7954cd64e50689d367145aa427
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-11 17:04:14 -0500

    Update restaurant model with accolades association

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,7 @@ class Restaurant < ActiveRecord::Base
   validates_presence_of :name, :address, :description
 
   has_many :events
+  has_many :awardings
+  has_many :accolades, through: :awardings
   belongs_to :city
 end
