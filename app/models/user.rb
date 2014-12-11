b4644c1d0af9c72171f3830f42f9b6f9292166b1
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-10 22:12:56 -0500

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name,
+    :email, :password, :address, :zipcode, :phone_number,
+    :referral_code, :membership_cost, :admin_status, :active_status
+  validates_uniqueness_of :email, :referral_code
+
+end
