33f31263d6a005da41649babf5aafba6439dbd0d
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-10 22:23:04 -0500

    Add missing comma to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  validates_presence_of :date, :number_of_seats
+  validates_presence_of :date, :number_of_seats,
     :seat_cost, :max_tickets_per_member
 
   belongs_to :restaurant
