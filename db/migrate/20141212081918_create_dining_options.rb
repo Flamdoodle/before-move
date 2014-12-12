b4532e5057b4c64432183f659dcfa14b49263bcb
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-12 03:28:42 -0500

    Fix dining options migration

--- db/migrate/20141212081918_create_dining_options.rb
+++ db/migrate/20141212081918_create_dining_options.rb
@@ -1,8 +1,8 @@
 class CreateDiningOptions < ActiveRecord::Migration
   def change
     create_table :dining_options do |t|
-      belongs_to :restaurant
-      belongs_to :experience
+      t.belongs_to :restaurant
+      t.belongs_to :experience
 
       t.timestamps
     end
