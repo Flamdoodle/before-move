550f585da781e2b269b1f7b72c27c64b0ad48d1e
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-12 18:06:31 -0500

    Update referrals migration - now includes name and email of person who was referred

--- db/migrate/20141212225308_create_referrals.rb
+++ db/migrate/20141212225308_create_referrals.rb
@@ -3,6 +3,8 @@ class CreateReferrals < ActiveRecord::Migration
     create_table :referrals do |t|
       t.belongs_to :user
       t.string :type
+      t.string :name_of_referred
+      t.string :email_of_referred
 
       t.timestamps
     end
