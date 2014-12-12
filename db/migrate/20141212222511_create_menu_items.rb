5129958a11276aa1c59fd747b2a09ac7913c2d03
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-12 17:28:54 -0500

    Update menu items migration

--- db/migrate/20141212222511_create_menu_items.rb
+++ db/migrate/20141212222511_create_menu_items.rb
@@ -1,6 +1,7 @@
 class CreateMenuItems < ActiveRecord::Migration
   def change
     create_table :menu_items do |t|
+      t.belongs_to :menu
       t.integer :course_number
       t.string :name
       t.string :description
