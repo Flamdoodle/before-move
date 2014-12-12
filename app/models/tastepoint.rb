eab57e4d2ffb04d2b1e4596b503f87da7c97cb05
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-12 18:02:50 -0500

    Setup tastepoint model with associations

---
+++ app/models/tastepoint.rb
@@ -0,0 +1,7 @@
+class Tastepoint < ActiveRecord::Base
+  validates_presence_of :amount, :earned_by
+  validates_uniqueness_of :earned_by
+
+  has_many :earnings
+  has_many :users, through: :earnings
+end
