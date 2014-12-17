c439cb0fc5fd3ac511ab20a13f2ca665ed93de85
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-17 14:21:52 -0500

    Add schema.rb

---
+++ db/schema.rb
@@ -0,0 +1,171 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141212225621) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "accolades", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "awardings", force: true do |t|
+    t.integer  "restaurant_id"
+    t.integer  "accolade_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bookings", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "number_of_tickets"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "cities", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.string   "email"
+    t.string   "phone_number"
+    t.boolean  "is_primary"
+    t.integer  "restaurant_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "dining_options", force: true do |t|
+    t.integer  "restaurant_id"
+    t.integer  "experience_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "earnings", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "tastepoint_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", force: true do |t|
+    t.date     "date"
+    t.integer  "number_of_seats"
+    t.integer  "restaurant_id"
+    t.decimal  "seat_cost"
+    t.integer  "max_tickets_per_member"
+    t.string   "non_member_code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "experiences", force: true do |t|
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inqueries", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "referral_source"
+    t.string   "zipcode"
+    t.date     "invite_sent_date"
+    t.string   "referral_code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "menu_items", force: true do |t|
+    t.integer  "menu_id"
+    t.integer  "course_number"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "menus", force: true do |t|
+    t.integer  "event_id"
+    t.integer  "number_of_courses"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "promo_codes", force: true do |t|
+    t.string   "code"
+    t.date     "expiration_date"
+    t.decimal  "membership_price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "referrals", force: true do |t|
+    t.integer  "user_id"
+    t.string   "type"
+    t.string   "name_of_referred"
+    t.string   "email_of_referred"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.integer  "city_id"
+    t.string   "neighborhood"
+    t.string   "cuisine_type"
+    t.string   "description"
+    t.decimal  "gratuity",     precision: 3, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tastepoints", force: true do |t|
+    t.integer  "amount"
+    t.string   "earned_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "address"
+    t.string   "zipcode"
+    t.string   "phone_number"
+    t.decimal  "dining_credit"
+    t.integer  "taste_points"
+    t.string   "referral_code"
+    t.integer  "membership_cost"
+    t.boolean  "admin_status"
+    t.boolean  "active_status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
