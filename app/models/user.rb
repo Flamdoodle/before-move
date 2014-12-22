40d90cd8f89caa2d3428fc4b50d287578b47468b
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-22 15:53:12 -0500

    Update user model to reflect migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   validates_presence_of :first_name, :last_name,
     :email, :password, :address, :zipcode, :phone_number,
-    :referral_code, :membership_cost, :admin_status, :active_status
+    :referral_code, :membership_cost, :is_active?, :is_admin?
   validates_uniqueness_of :email, :referral_code
 
   has_many :bookings
