0068abf367edc71a7270ad5bb429a47a82d1f1c3
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-22 17:19:20 -0500

    Fix user model to read booleans correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   validates_presence_of :first_name, :last_name,
     :email, :password, :address, :zipcode, :phone_number,
-    :referral_code, :membership_cost, :is_active?, :is_admin?
+    :referral_code, :membership_cost
   validates_uniqueness_of :email, :referral_code
+  validates :is_admin?, inclusion: { in: [true, false] }
+  validates :is_active?, inclusion: { in: [true, false] }
 
   has_many :bookings
   has_many :events, through: :bookings
