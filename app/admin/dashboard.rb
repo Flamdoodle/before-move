42f46d503f11b19a6fa9df3524654a80c6b03a27
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-21 21:09:43 -0500

    Add initial structure for admin dashboard panel

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,13 +3,40 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  # div class: "blank_slate_container", id: "dashboard_default_message" do
+  #   span class: "blank_slate" do
+  #     span I18n.t("active_admin.dashboard_welcome.welcome")
+  #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  #   end
+  # end
+
+    columns do
+      column do
+        panel "Important Info" do
+          # Important info here
+        end
+
+        panel "Upcoming Events" do
+          # Upcoming events here
+        end
+
+        panel "Past Due Users" do
+          # Past due users here
+        end
+      end
+
+      column do
+        panel "Last 100 Logins" do
+          # Last 100 logins
+        end
+      end
+
+      column do
+        panel "Latest Invites" do
+          # Last X invites
+        end
       end
     end
-
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
