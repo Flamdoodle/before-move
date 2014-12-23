4e217121b64aff79972ff09d133baf488014b0d4
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-23 18:22:14 -0500

    Clean up some active admin comments

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -2,19 +2,6 @@ ActiveAdmin.register Event do
   permit_params :time, :date, :number_of_seats, :restaurant_id, :seat_cost, :max_tickets_per_member, :nonmember_code,
     menus_attributes: [:name, :number_of_courses, :description, :id, menu_items_attributes: [:ide, :course_number, :name, :description]]
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
   form do |f|
     f.inputs "Event Details" do
       f.input :restaurant
