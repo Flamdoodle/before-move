891abefc893386ea3f851b29d3e9c75c1240e75e
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-23 16:21:08 -0500

    Add menu and menu items to event form

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -14,5 +14,29 @@ ActiveAdmin.register Event do
   #   permitted
   # end
 
+  form do |f|
+    f.inputs "Event Details" do
+      f.input :restaurant
+      f.input :date
+      f.input :number_of_seats
+      f.input :seat_cost
+      f.input :max_tickets_per_member
+      f.input :nonmember_code
+    end
+    f.inputs do
+      f.has_many :menus, heading: "Menu" do |cf|
+        cf.input :name
+        cf.input :number_of_courses
+        cf.input :description
+        cf.has_many :menu_items do |ccf|
+          ccf.input :course_number
+          ccf.input :name
+          ccf.input :description
+        end
+      end
+    end
+    f.actions
+  end
+
 
 end
