9f491576702db7614ddaec9ef65048c30977f336
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-24 02:59:28 -0500

    Update experience and dining option models to fix association errors

--- app/models/dining_option.rb
+++ app/models/dining_option.rb
@@ -1,4 +1,6 @@
 class DiningOption < ActiveRecord::Base
   has_many :experiences
   belongs_to :restaurant
+
+  accepts_nested_attributes_for :experiences
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,3 @@
 class Experience < ActiveRecord::Base
   belongs_to :dining_options
-
-  accepts_nested_attributes_for :restaurants, :dining_options
 end
