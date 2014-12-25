1d5364fd390079f4c039ad7046bc4b72e0707395
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-25 17:15:00 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218174839) do
+ActiveRecord::Schema.define(version: 20141225220740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,13 @@ ActiveRecord::Schema.define(version: 20141218174839) do
   create_table "earnings", force: true do |t|
     t.integer  "user_id"
     t.integer  "tastepoint_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "event_benefits", force: true do |t|
+    t.string   "benefit"
+    t.integer  "benefit_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
