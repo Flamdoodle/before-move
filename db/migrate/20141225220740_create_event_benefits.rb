37c870f12c0c540618d883d06ec7eba68826d068
Stephen Roth <stephenroth.sr@gmail.com>
2014-12-25 17:14:26 -0500

    Fix error in event benefit migration

--- db/migrate/20141225220740_create_event_benefits.rb
+++ db/migrate/20141225220740_create_event_benefits.rb
@@ -2,7 +2,7 @@ class CreateEventBenefits < ActiveRecord::Migration
   def change
     create_table :event_benefits do |t|
       t.string :benefit
-      t.belongs_to :benefit
+      t.belongs_to :event
 
       t.timestamps
     end
