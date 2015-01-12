13781e6bc79145c6f863c57dda4d37ed9f95414e
Stephen Roth <stephenroth.sr@gmail.com>
2015-01-12 18:02:52 -0500

    Fix nested_attributes_for in restaurant space option model

--- app/models/restaurant_space_option.rb
+++ app/models/restaurant_space_option.rb
@@ -3,5 +3,5 @@ class RestaurantSpaceOption < ActiveRecord::Base
   belongs_to :space_option
   has_many :events, through: :space_option
 
-  accepts_nested_attributes_for :space_options
+  accepts_nested_attributes_for :space_option
 end
