3f604be180f7567f9be6425b1c39d47411c5ebbd
Stephen Roth <stephenroth.sr@gmail.com>
2015-01-12 17:21:02 -0500

    Update space_options to exclude restaurant specific info

--- db/migrate/20141212081316_create_space_options.rb
+++ db/migrate/20141212081316_create_space_options.rb
@@ -2,8 +2,6 @@ class CreateSpaceOptions < ActiveRecord::Migration
   def change
     create_table :space_options do |t|
       t.string :space_option
-      t.decimal :minimum_spend
-      t.integer :number_of_seats
 
       t.timestamps
     end
