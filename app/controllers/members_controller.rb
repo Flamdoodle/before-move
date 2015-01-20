3d2be1ea84b198b8aef95efce6e083dd0e8a6ab7
Stephen Roth <stephenroth.sr@gmail.com>
2015-01-20 17:28:03 -0500

    Create members controller and inital member views

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,22 @@
+class MembersController < ApplicationController
+  def new
+    @member = Member.new
+  end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      session[:user_id] = @member.id
+      redirect_to root_path
+    else
+      flash[:error] = @member.errors.full_messages.join(" & ")
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone_number, :zipcode)
+  end
+end
