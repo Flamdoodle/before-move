1dd13f0bb744569e6b3090ebd1c5c951eeffe60c
Stephen Roth <stephenroth.sr@gmail.com>
2015-01-20 17:27:33 -0500

    Update member model to include before_validation method to create referral_code

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,12 +1,14 @@
 class Member < ActiveRecord::Base
   has_secure_password
 
+  before_validation :create_referral_code, on: :create
+
   validates_presence_of :first_name, :last_name,
-    :email, :password, :street_address, :city, :state, :zipcode, :phone_number,
-    :referral_code, :membership_cost
+    :email, :password, :zipcode, :phone_number, :referral_code
+    # :city, :state, :membership_cost, :street_address
   validates_uniqueness_of :email, :referral_code
-  validates :is_admin?, inclusion: { in: [true, false] }
-  validates :is_active?, inclusion: { in: [true, false] }
+  # validates :is_admin?, inclusion: { in: [true, false] }
+  # validates :is_active?, inclusion: { in: [true, false] }
 
   has_many :bookings
   has_many :events, through: :bookings
@@ -18,5 +20,20 @@ class Member < ActiveRecord::Base
 
   def name
     "#{self.first_name} #{self.last_name}"
+  end
+
+  protected
+
+  def create_referral_code(counter = 0)
+    p "in the method"
+    counter += 1
+    referral_code = "#{self.first_name}#{self.last_name}#{counter}"
+    if Member.find_by(referral_code: referral_code)
+      p "in the if"
+      create_referral_code(counter)
+    else
+      p "in the else"
+      self.referral_code = referral_code
+    end
   end
 end
