b986fe5e195bbc54cba0aec100ad12c983e5767e
Stephen Roth <stephenroth.sr@gmail.com>
2015-01-21 14:54:34 -0500

    Fix unexpected return error in various index pages

--- app/admin/inquery.rb
+++ app/admin/inquery.rb
@@ -6,9 +6,14 @@ ActiveAdmin.register Inquery do
     column(:zipcode)
     column(:referred_by) do |inquery|
       referred_by = Member.find_by(referral_code: inquery.referral_code)
-      return link_to(referred_by.name, admin_member_path(referred_by)) if referred_by
-      referred_by ||= PromoCode.find_by(code: inquery.referral_code)
-      return "#{referred_by.source}" if referred_by
+      if referred_by
+        link_to(referred_by.name, admin_member_path(referred_by))
+      else
+        referred_by ||= PromoCode.find_by(code: inquery.referral_code)
+        if referred_by
+          "#{referred_by.source}"
+        end
+      end
     end
     column(:created_at)
   end
