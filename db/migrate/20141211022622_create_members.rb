abe7ee767902472d532e2268671de42be859519b
Stephen Roth <stephenroth.sr@gmail.com>
2015-01-26 18:16:44 -0500

    Add facebook_token and expiration date to member table

--- db/migrate/20141211022622_create_members.rb
+++ db/migrate/20141211022622_create_members.rb
@@ -18,6 +18,8 @@ class CreateMembers < ActiveRecord::Migration
       t.boolean :is_active?, default: true, null: false
       t.datetime :last_login
       t.string :code_used_at_signup
+      t.string :facebook_token
+      t.datetime :facebook_token_expires_at
 
       t.timestamps
     end
