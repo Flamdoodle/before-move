675bcfe2430da7d39a264491e0c2e3e53d310d8b
Stephen Roth <stephenroth.sr@gmail.com>
2015-01-26 18:16:16 -0500

    Fix omni auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   ActiveAdmin.routes(self)
   root 'welcome#home'
   # root 'members#new'
@@ -9,4 +10,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+
+  # OmniAuth
+  get 'auth/facebook/callback', to: 'sessions#create_facebook'
+  get 'auth/failure', to: redirect('/')
 end
