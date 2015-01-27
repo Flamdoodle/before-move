cb7a4c86cc468e73eb04f3cfafebe8064893de43
Stephen Roth <stephenroth.sr@gmail.com>
2015-01-26 22:11:34 -0500

    Add tweet method to member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -23,6 +23,17 @@ class Member < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
 
+  def tweet(tweet)
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = self.twitter_token
+      config.access_token_secret = self.twitter_token_secret
+    end
+
+    client.update(tweet)
+  end
+
   protected
 
   def create_referral_code(counter = 0)
