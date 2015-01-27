34d3e6575e2d3bccfda6cae29c7af39cc1db68be
Stephen Roth <stephenroth.sr@gmail.com>
2015-01-26 19:35:00 -0500

    Add create_facebook method to sessions controller - this might be moved to the members controller later

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,14 @@ class SessionsController < ApplicationController
     end
   end
 
+  def create_facebook
+    auth = env["omniauth.auth"]
+    current_member.facebook_token = auth.credentials.token
+    current_member.facebook_token_expires_at = Time.at(auth.credentials.expires_at)
+    flash[:error] = current_member.errors.full_messages unless current_member.save
+    redirect_to root_path
+  end
+
   def destroy
     session.clear
     redirect_to root_path
