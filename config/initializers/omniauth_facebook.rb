4cf33adcdf96d676144ee6d3378cb507de37c338
Stephen Roth <stephenroth.sr@gmail.com>
2015-01-26 22:13:32 -0500

    Rename facebook omniauth initilizer:

--- config/initializers/omniauth_facebook.rb
+++ config/initializers/omniauth_facebook.rb
@@ -2,5 +2,4 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
-  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
 end
