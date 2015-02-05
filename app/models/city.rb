bdd0ea99a5db3e474799399cdf7f723ba240fe06
Stephen Roth <stephenroth.sr@gmail.com>
2015-02-05 15:39:57 -0500

    Update city model and migration to reflect new states assocation

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,7 @@
 class City < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  validates :state, presence: true, uniqueness: true
+  validates :state, presence: true
 
+  belongs_to :state
   has_many :restaurants
 end
