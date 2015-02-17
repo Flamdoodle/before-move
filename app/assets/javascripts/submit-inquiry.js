c0441f3151657d7fc236882caa0b092afa1243b4
Stephen Roth <stephenroth.sr@gmail.com>
2015-02-17 04:09:29 -0500

    Add JS to inquiry modal to hide/show referral code

--- app/assets/javascripts/submit-inquiry.js
+++ app/assets/javascripts/submit-inquiry.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    $(".has-referral-code").click(function(e) {
+        e.preventDefault();
+
+        $(".has-referral-code").hide();
+        $(".invisible").removeClass("invisible");
+    })
+
     $(".submit-inquiry").click(function(e) {
         e.preventDefault();
 
